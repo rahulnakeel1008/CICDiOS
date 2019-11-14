88a12fd41825a2f96dd71d27e152c4866a0625e2
Rahul <Rahul@SL-SP-LAP-0449.local>
2019-11-14 19:07:44 +0530

    added events

--- RetirementCalculator/ViewController.swift
+++ RetirementCalculator/ViewController.swift
@@ -7,14 +7,47 @@
 //
 
 import UIKit
+import AppCenterCrashes
+import AppCenterAnalytics
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var monthlyInvestmentsTextField: UITextField!
+    @IBOutlet weak var ageTextField: UITextField!
+    @IBOutlet weak var retirementAgeTextField: UITextField!
+    @IBOutlet weak var interestRateTextField: UITextField!
+    @IBOutlet weak var savingsTextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        if MSCrashes.hasCrashedInLastSession(){
+            
+            let alert = UIAlertController(title: "Ooops", message: "Error occured", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+            
+            self.present(alert, animated: true, completion: nil)
+            
+        }
+    }
 
+    @IBAction func calculateButton_TouchUpInside(_ sender: Any) {
+//         MSCrashes.generateTestCrash()
+        
+        let current_age : Int? = Int(ageTextField.text!)
+           let planned_retirement_age : Int? = Int(retirementAgeTextField.text!)
+           
+           let properties = ["current_age": String(current_age!),
+                             "planned_retirement_age": String(planned_retirement_age!)]
+           
+           MSAnalytics.trackEvent("calculate_retirement_amount", withProperties: properties)
+     }
+     
 
 }
 
